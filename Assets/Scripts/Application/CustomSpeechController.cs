7e8125ddca1b040c96f95d1719d5af4c8f669e2c
Fabiano <rizadom1@gmail.com>
2023-10-16 06:02:34 -0300

    [Improve] Refactored code / Added States to give better feedback / removed echo

--- Assets/Scripts/Application/CustomSpeechController.cs
+++ Assets/Scripts/Application/CustomSpeechController.cs
@@ -3,22 +3,42 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SocialPlatforms;
+using UnityEngine.UI;
 using Whisper;
+using Whisper.Utils;
 
 public class CustomSpeechController : MonoBehaviour
 {
-    private readonly string textToSpeakFormat = "           {0}";
-
+    [Header("Controller")]
     [SerializeField] private MicrofoneController microfoneController;
+
     [Space]
+    [SerializeField] private Button recordButton;
+    [SerializeField] private TextMeshProUGUI recordTextButton;
+
+    [Space]
+    [SerializeField] private TMP_InputField inputTextToSpeak;
     [SerializeField] private TextMeshProUGUI textToSpeak;
     [SerializeField] private TextMeshProUGUI resultText;
 
     private SpeechEvaluator speechEvaluator = new SpeechEvaluator();
+    private State currentState = State.Ready;
+
+    private enum State
+    {
+        Ready,
+        Recording,
+        Verifying,
+        VoiceNotRecognized,
+        VerifyingError
+    }
 
     private void Awake()
     {
+        recordButton.onClick.AddListener(OnRecordButtonPressed);
         microfoneController.whisper.OnNewSegment += OnNewSegment;
+        microfoneController.record.OnRecordStop += OnRecordStop;
     }
 
     /// <summary>
@@ -43,18 +63,68 @@ public class CustomSpeechController : MonoBehaviour
         {
             resultText.text = "Você está no caminho certo, ouça o audio de exemplo e tente novamente!";
         }
+
+        SetState(State.Ready);        
     }
 
-    //public string word1;
-    //public string word2;
+    private void OnRecordButtonPressed()
+    {
+        if (!microfoneController.record.IsRecording)
+        {
+            SetState(State.Recording);
+        }
+        else
+        {
+            SetState(State.Verifying);
+        }
+    }
 
-    //StringSpeechCompare stringSpeechCompare = new StringSpeechCompare(); 
+    private async void OnRecordStop(AudioChunk recordedAudio)
+    {
+        var res = await microfoneController.whisper.GetTextAsync(recordedAudio.Data, recordedAudio.Frequency, recordedAudio.Channels);
+        if (string.IsNullOrEmpty(res.Result))
+        {
+            SetState(State.VoiceNotRecognized);
+            return;
+        }
+        else if (res == null)
+        {
+            SetState(State.VerifyingError);
+            return;
+        }
+    }
 
-    //private void Update()
-    //{
-    //    if (Input.GetKeyUp(KeyCode.A))
-    //    {
-    //        Debug.Log(stringSpeechCompare.LevenshteinDistance(word1, word2));
-    //    }
-    //}
+    private void SetState(State newState)
+    {
+        if(newState == State.Ready)
+        {
+            inputTextToSpeak.interactable = true;
+            recordButton.interactable = true;
+            recordTextButton.text = "Pressione para falar";
+        } 
+        else if (newState == State.Recording)
+        {
+            microfoneController.OnRecordStart();
+            recordTextButton.text = "Verificar";
+        }
+        else if (newState == State.Verifying)
+        {
+            microfoneController.OnRecordEnd();
+            inputTextToSpeak.interactable = false;
+            recordButton.interactable = false;
+            recordTextButton.text = "Verificando";
+        }
+        else if (newState == State.VoiceNotRecognized)
+        {
+            SetState(State.Ready);
+            resultText.text = "Nenhum som foi reconhecido. Tente novamente";
+        }
+        else if (newState == State.VerifyingError)
+        {
+            SetState(State.Ready);
+            resultText.text = "Houve um erro no processamento do som. Tente novamente";
+        }
+
+        currentState = newState;
+    }
 }
--- Assets/Scripts/Application/MicrofoneController.cs
+++ Assets/Scripts/Application/MicrofoneController.cs
@@ -6,79 +6,20 @@ using UnityEngine.UI;
 using Whisper.Utils;
 using Whisper;
 using TMPro;
+using System;
 
 public class MicrofoneController : MonoBehaviour
 {
     public WhisperManager whisper;
-    public MicrophoneRecord microphoneRecord;
-    public bool streamSegments = true;
+    public MicrophoneRecord record;
 
-    [Header("UI")]
-    [SerializeField] private Button recordButton;
-    [SerializeField] private TextMeshProUGUI recordTextButton;
-    [SerializeField] private Toggle translateToggle;
-    [SerializeField] private Toggle vadToggle;
-
-    private void Awake()
+    public void OnRecordStart()
     {
-        whisper.OnNewSegment += OnNewSegment;
-
-        microphoneRecord.OnRecordStop += OnRecordStop;
-
-        recordButton.onClick.AddListener(OnButtonPressed);
-        //translateToggle.isOn = whisper.translateToEnglish;
-        //translateToggle.onValueChanged.AddListener(OnTranslateChanged);
-
-        //vadToggle.isOn = microphoneRecord.vadStop;
-        //vadToggle.onValueChanged.AddListener(OnVadChanged);
+        record.StartRecord();
     }
 
-    private void OnVadChanged(bool vadStop)
+    public void OnRecordEnd()
     {
-        microphoneRecord.vadStop = vadStop;
-    }
-
-    private void OnButtonPressed()
-    {
-        if (!microphoneRecord.IsRecording)
-        {
-            microphoneRecord.StartRecord();
-            //recordButton.interactable = false;
-            recordTextButton.text = "Verificar";
-        }
-        else
-        {
-            microphoneRecord.StopRecord();
-            //recordButton.interactable = true;
-            recordTextButton.text = "Pressione para falar";
-        }
-    }
-
-    private async void OnRecordStop(AudioChunk recordedAudio)
-    {
-        //recordButton.interactable = true;
-        recordTextButton.text = "Verificar";
-
-        var sw = new Stopwatch();
-        sw.Start();
-
-        var res = await whisper.GetTextAsync(recordedAudio.Data, recordedAudio.Frequency, recordedAudio.Channels);
-        if (res == null)
-            return;
-        
-        var text = res.Result;
-    }
-
-    private void OnTranslateChanged(bool translate)
-    {
-        whisper.translateToEnglish = translate;
-    }
-
-    private void OnNewSegment(WhisperSegment segment)
-    {
-        if (!streamSegments)
-            return;
-
-         // segment.Text; -> spokenText
+        record.StopRecord();
     }
 }
